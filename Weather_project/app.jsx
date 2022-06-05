40a5b93123a572677de4e0eda6340fde9e5e9003
krishmk1009 <76839752+krishmk1009@users.noreply.github.com>
2022-06-05 23:57:21 +0530

    Create app.jsx

---
+++ Weather_project/app.jsx
@@ -0,0 +1,15 @@
+import "./styles.css";
+import React, { useState } from "react";
+
+import Top from "./components/top";
+import Bottom from "./components/bottom";
+
+function App() {
+  return (
+    <div className="app">
+      <Top />
+    </div>
+  );
+}
+
+export default App;
